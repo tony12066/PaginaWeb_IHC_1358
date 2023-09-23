0256ce70e2b4855e9d1fc346b1bd5936ebc85997
Anthony <mark.arribasplata@upn.edu.pe>
2023-09-23 16:40:25 -0500

    Version 2 - Se agrego funciones js

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,9 @@
 function Saludar() {
-    
+    var nombre=document.getElementById('txtNombre').value;
+    alert("Hola "+nombre+", bienvenido!!");
+}
+function Resetear() {
+    var txtNombre=document.getElementById('txtNombre');
+    txtNombre.value="";
+    txtNombre.focus();
 }
